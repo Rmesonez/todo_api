fd3ae3d0884cfaa6ceb33e08f8b3ad84a6fed2a8
Rmesonez <rmesonez@gmail.com>
2023-05-13 12:45:17 -0600

    first

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const usersRoutes = require('./routes/users.routes.js');
 const tasksRoutes = require('./routes/tasks.routes.js');
 const categoriesRoutes = require('./routes/categories.routes.js');
 const subCategoriesRoutes = require('./routes/subcategories.routes.js');
-const TasksSubCategoriesRoutes = require('./routes/tasks_subcategories.routes.js');
+const tasksSubCategoriesRoutes = require('./routes/tasks_subcategories.routes.js');
 const authRoutes = require('./routes/auth.routes.js');
 require('dotenv').config();
 
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4000;
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(usersRoutes, tasksRoutes, categoriesRoutes, subCategoriesRoutes, TasksSubCategoriesRoutes, authRoutes);
+app.use(usersRoutes, tasksRoutes, categoriesRoutes, subCategoriesRoutes, tasksSubCategoriesRoutes, authRoutes);
 
 async function testConnection() {
     try {
